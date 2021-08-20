69c5a3c0525d2266669ea147da19d21e2359047e
vasanthakumaaran <vasanthakumaaran76@gmail.com>
2021-08-20 13:52:14 +0530

    added path by from os

--- mysite_v/mysite/settings.py
+++ mysite_v/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from os import path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    'static'
+    path.join(BASE_DIR,'static')
 ]
 
 # Default primary key field type
